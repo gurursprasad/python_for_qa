44b8d631ce4805b516b6c87fab1c6186f6122ac4
guru-rs-prasad-01 <v-guru@exostellar.io>
2025-08-10 08:38:18 +0530

    reserved words

--- 02_language_fundamentals/01_python_identifiers.py
+++ 02_language_fundamentals/01_python_identifiers.py
@@ -5,20 +5,31 @@
     Class name
     Or any other user-defined name.
 
-2. Rules for writing Identifiers:
-    It can only contain letters, numbers and underscores
-    They must start with a letter or underscore
-    They cannot start with a number
-    Identifiers are case-sensiutive
-    We cannot use Python keywords as identifiers
+2. Rules for writing identifiers:
+    Contain letters, numbers and underscores
+    It can start with letter or underscore
+    It cannot start with a number
+    They are case sensitive
+    CAnnot use python keywords as identifiers
 """
 
 a = 10
 
-def test():
+def test_1():
     pass
 
-class Test:
+class Test1:
     pass
 
+__a1__ = 10
+_a2 = 2
 
+# 2a = 40 # Invalid
+
+total = 99
+TOTAL = 100
+
+print(total)
+print(TOTAL)
+
+# if = 100
---
+++ 02_language_fundamentals/02_reserved_words.py
@@ -0,0 +1,15 @@
+"""
+In Python some words are reserved to represent some meaning or functionality.
+Such types of words are called reserved words.
+
+There are 30 - 35 reserved words available in Python:
+    True, False, None
+    and, or ,not,is
+    if, elif, else
+    while, for, break, continue, return, in, yield
+    try, except, finally, raise, assert
+    import, from, as, class, def, pass, global, nonlocal, lambda, del, with
+"""
+
+
+# elif = "hello"
